07d7ee16dc493561158f737bb1bd9d78677a81f5
Tom Scott <tscott@weblinc.com>
2018-05-12 18:13:09 -0400

    Update test descriptions

--- test/index-test.js
+++ test/index-test.js
@@ -1,19 +1,23 @@
 import postmaker from "../src/index"
 import rimraf from "rimraf"
+import { existsSync } from "fs"
 
-test('generates article', () => {
-  const params = {
-    title: "Foo",
-    category: "foos",
-    tags: ["foo", "bar"],
-    path: "./tmp"
-  }
-  const post = postmaker(params)
+describe('postmaker', () => {
+  test('generates article', () => {
+    const params = {
+      title: "Foo",
+      category: "foos",
+      tags: ["foo", "bar"],
+      path: "./tmp"
+    }
+    const post = postmaker(params)
 
-  expect(post.title).toEqual(params.title)
-  expect(post.category).toEqual(params.category)
-  expect(post.tags).toEqual(params.tags)
-  expect(post.filename).toMatch(params.path)
+    expect(post.title).toEqual(params.title)
+    expect(post.category).toEqual(params.category)
+    expect(post.tags).toEqual(params.tags)
+    expect(post.filename).toMatch(params.path)
+    expect(existsSync(post.filename)).toBe(true)
+  })
+
+  afterEach(() => rimraf.sync('./tmp/*.md'))
 })
-
-afterEach(() => rimraf.sync('./tmp/*.md'))
--- test/post-test.js
+++ test/post-test.js
@@ -1,40 +1,67 @@
+/* eslint-env jest */
+
 import Post from '../src/post'
 import { includes, trim } from 'lodash'
 import { dump } from 'js-yaml'
 import rimraf from "rimraf"
 
-test('template', () => {
-  const frontMatter = trim(dump({ title: 'test' })),
-        compiled = Post.template({ frontMatter })
+const params = {
+  title: 'foo bar baz',
+  category: 'gbs',
+  tags: [
+    'foo',
+    'bar',
+    'baz'
+  ],
+  path: './tmp'
+},
+post = new Post(params)
 
-  expect(compiled).toMatch(/title: test/)
-  expect(compiled).toMatch(/Write the article here/)
+describe('Post.template', () => {
+  test('interpolates the template source into a Markdown article', () => {
+    const frontMatter = trim(dump({ title: 'test' })),
+          compiled = Post.template({ frontMatter })
+
+    expect(compiled).toMatch(/title: test/)
+    expect(compiled).toMatch(/Write the article here/)
+  })
 })
 
-test('creation', () => {
-  const params = {
-    title: 'foo bar baz',
-    category: 'gbs',
-    tags: [
-      'foo',
-      'bar',
-      'baz'
-    ],
-    path: './tmp'
-  },
-  post = new Post(params)
+describe('Post.now', () => {
+  test('slashes returns the date formatted with slashes', () => {
+    expect(Post.now.slashes).toMatch('/')
+  })
 
-  expect(post.title).toEqual(params.title)
-  expect(post.category).toEqual(params.category)
-  expect(post.tags).toEqual(params.tags)
-  expect(post.filepath).toEqual(params.path)
-  expect(post.id).toEqual('foo-bar-baz')
-  expect(post.path).toEqual(`/${Post.now.slashes}/foo-bar-baz`)
-  expect(post.filename).toEqual(`${params.path}/${Post.now.hyphens}-foo-bar-baz.md`)
-  expect(post.attributes.date).toEqual(Post.now.slashes)
-  expect(post.metadata).toMatch(/title: foo bar baz/)
-  expect(post.contents).toMatch(post.metadata)
-  expect(post.write()).toBe(true)
+  test('hyphens returns the date formatted with hyphens', () => {
+    expect(Post.now.hyphens).toMatch('-')
+  })
 })
 
-afterEach(() => rimraf.sync('./tmp/*.md'))
+describe('Post', () => {
+  test('save passed-in parameters', () => {
+    expect(post.title).toEqual(params.title)
+    expect(post.category).toEqual(params.category)
+    expect(post.tags).toEqual(params.tags)
+    expect(post.filepath).toEqual(params.path)
+  })
+
+  test('parameterize title into id', () => {
+    expect(post.id).toEqual('foo-bar-baz')
+  })
+
+  test('build path from id and slashed date', () => {
+    expect(post.path).toEqual(`/${Post.now.slashes}/foo-bar-baz`)
+  })
+
+  test('build filename from passed-in root path, hyphened date, and id', () => {
+    expect(post.filename).toEqual(`./tmp/${Post.now.hyphens}-foo-bar-baz.md`)
+  })
+
+  test('build attributes hash from article metadata and slashed date', () => {
+    expect(post.attributes.title).toEqual(params.title)
+    expect(post.attributes.date).toEqual(Post.now.slashes)
+  })
+
+  afterEach(() => rimraf.sync('./tmp/*.md'))
+})
+
